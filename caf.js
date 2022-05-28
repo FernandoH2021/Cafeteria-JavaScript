c38a97ce8af24c6286f03981883b246db4005ed0
Fernando <fh1273509@gmail.com>
2022-05-27 22:14:42 -0300

    definiendo variables js

---
+++ caf.js
@@ -0,0 +1,30 @@
+(function(){
+    /*variables*/
+
+    var datos = document.getElementById("datos");
+
+    var date = document.getElementById("date");
+    var nombre = document.getElementById("nombre");
+    var tiempo = document.getElementById("tiempo");
+
+    var email = document.getElementById("email");
+    var number = document.getElementById("number");
+    var telefono = document.getElementById("telefono");
+
+    /*funciones*/
+    var agregarDatos = function(){
+        var dato = agregarNombre.Value;
+        nuevoDato = document.createElement("date");
+        nuevoNombre = document.createElement("nombre");
+        nuevoTiempo = document.createElement("tiempo");
+        nuevoEmail = document.createElement("email");
+        nuevoNumero = document.createElement("number");
+        nuevoTelefono = document.createElement("telefono");
+
+
+
+
+    }
+
+
+})

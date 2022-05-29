27032135d93f895c4c46012a03e7cc7b235db6cb
Fernando <fh1273509@gmail.com>
2022-05-29 20:36:26 -0300

    corrijiendo formulario de contacto

--- caf.js
+++ caf.js
@@ -1,34 +1,10 @@
-(function(){
-    /*variables*/
-
-    var date = document.getElementById("date");
-    var nombre = document.getElementById("nombre");
-    var tiempo = document.getElementById("tiempo");
-
-    var email = document.getElementById("email");
-    var number = document.getElementById("number");
-    var telefono = document.getElementById("telefono");
-
-    /*funciones*/
-    var agregarDatos = function(){
-        var dato = agregarNombre.Value,
-        nuevoDato = document.createElement("date"),
-        nuevoNombre = document.createElement("nombre"),
-        nuevoTiempo = document.createElement("tiempo"),
-        nuevoEmail = document.createElement("email"),
-        nuevoNumero = document.createElement("number"),
-        nuevoTelefono = document.createElement("telefono");
-
-    if(nombre === ""){
-        agregarNombre.setAttribute("Reservar");
-        agregarNombre.clasName = "error";
-        return false;
-        
-
+var inputs = document.getElementsByClassName('formulario__input');
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener('keyup', function(){
+    if(this.value.length>=1) {
+      this.nextElementSibling.classList.add('fijar');
+    } else {
+      this.nextElementSibling.classList.remove('fijar');
     }
-
-
-    }
-
-
-});
+  });
+}

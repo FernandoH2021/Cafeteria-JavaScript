756acbfd8d251e2780367b35c7dac4acb8992c88
Fernando <fh1273509@gmail.com>
2022-05-28 22:40:31 -0300

    corrigiendo js

--- caf.js
+++ caf.js
@@ -1,8 +1,6 @@
 (function(){
     /*variables*/
 
-    var datos = document.getElementById("datos");
-
     var date = document.getElementById("date");
     var nombre = document.getElementById("nombre");
     var tiempo = document.getElementById("tiempo");
@@ -13,18 +11,24 @@
 
     /*funciones*/
     var agregarDatos = function(){
-        var dato = agregarNombre.Value;
-        nuevoDato = document.createElement("date");
-        nuevoNombre = document.createElement("nombre");
-        nuevoTiempo = document.createElement("tiempo");
-        nuevoEmail = document.createElement("email");
-        nuevoNumero = document.createElement("number");
+        var dato = agregarNombre.Value,
+        nuevoDato = document.createElement("date"),
+        nuevoNombre = document.createElement("nombre"),
+        nuevoTiempo = document.createElement("tiempo"),
+        nuevoEmail = document.createElement("email"),
+        nuevoNumero = document.createElement("number"),
         nuevoTelefono = document.createElement("telefono");
 
+    if(nombre === ""){
+        agregarNombre.setAttribute("Reservar");
+        agregarNombre.clasName = "error";
+        return false;
+        
 
+    }
 
 
     }
 
 
-})
+});
